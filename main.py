3fa1cff4ab7ab797f111f3afdf7f68a6d0feff0c
Roman Simmons <simmorom000@warren.k12.in.us>
2024-10-21 08:44:02 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# TODO: Use the input function for card1
+#  Use the input function for card2
+# Dont forget int
+
+# TODO: Make a variable card_sum and
+#  set it equal to the sum of the cards
+
+is_21_or_over= # TODO: use your relational operator
+
+# TODO: print the result
+int(input("card1"))
+int(input("card2"))
+card_sum = 21
+is_21_or_over= card1
